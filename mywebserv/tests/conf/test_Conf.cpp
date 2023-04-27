0f5a90d44bd67218e49539822bbc4250cd038259
chanma <yunosuke.homma@icloud.com>
2023-04-28 03:41:20 +0900

    testを追加、2のほうがバグってる

--- mywebserv/tests/conf/test_Conf.cpp
+++ mywebserv/tests/conf/test_Conf.cpp
@@ -1,14 +1,40 @@
 #define BOOST_TEST_MODULE ConfTest
 #include <boost/test/included/unit_test.hpp>
 #include "Conf.hpp"
+#include "ConfParser.hpp"
 
-BOOST_AUTO_TEST_CASE(test_conf)
+#define PATH_TO_TESTCONF "../../../tests/conf/test_config.txt"
+
+const char* CONF_DATA = R"(server {
+    listen 80;
+    server_name example.com www.example.com;
+
+    location / {
+        root /var/www/html;
+    }
+
+    location /images/ {
+        root /var/www/images;
+    }
+})";
+
+BOOST_AUTO_TEST_CASE(test_conf_1)
         {
                 // Confオブジェクトを作成
-                Conf conf;
+                ConfParser test1;
 
         // テストを行う
-        // 例えば、ConfクラスにgetValue()というメソッドがあると仮定して
-        int expected_value = 42;
-        BOOST_CHECK_EQUAL(conf.getValue(), expected_value);
+        // testsまでの階層に注意
+          BOOST_REQUIRE(test1.readFile(PATH_TO_TESTCONF) == true);
+          BOOST_CHECK_EQUAL(test1.getFileData(), CONF_DATA);
+        }
+
+BOOST_AUTO_TEST_CASE(test_conf_2)
+        {
+                // Confオブジェクトを作成
+                ConfParser test2;
+
+                test2.setFileData("");
+                BOOST_CHECK_EQUAL(test2.getWord(), "server");
+                std::cout << "test2.getWord(): " << test2.getWord() << std::endl;
         }
