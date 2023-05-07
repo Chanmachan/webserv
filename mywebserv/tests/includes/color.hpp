95b5be29121a12ec6d97214644f2f51aad635321
chanma <yunosuke.homma@icloud.com>
2023-05-07 16:04:01 +0900

    便利そうだから足しといた

---
+++ mywebserv/tests/includes/color.hpp
@@ -0,0 +1,17 @@
+//
+// Created by chanma on 2023/05/02.
+//
+
+#ifndef MYPROJECT_COLOR_HPP
+#define MYPROJECT_COLOR_HPP
+
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN "\x1b[36m"
+#define GRAY "\x1b[37m"
+#define RESET "\x1b[0m"
+
+#endif //MYPROJECT_COLOR_HPP
