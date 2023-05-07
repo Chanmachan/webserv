848851fba32f4d5455878d2eb1b33d198488d1ba
chanma <yunosuke.homma@icloud.com>
2023-05-07 21:07:30 +0900

    二重インクルードとかしちゃいそうだからインクルードをまとめるためだけのヘッダー

---
+++ mywebserv/srcs/server/server.hpp
@@ -0,0 +1,15 @@
+//
+// Created by chanma on 2023/05/07.
+//
+
+#ifndef MYPROJECT_SERVER_HPP
+#define MYPROJECT_SERVER_HPP
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <fcntl.h>
+#include <sys/epoll.h>
+
+#endif //MYPROJECT_SERVER_HPP
